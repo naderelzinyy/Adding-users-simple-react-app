3ee81bc1ee46623afac0ac1c5657735b38fb86ea
naderelzinyy <naderelziny805@gmail.com>
2021-08-18 13:04:29 +0300

    Creating and connecting the error modal

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import classes from './ErrorModal.module.css'
+import Card from './Card'
+import Button from './Button.js'
+
+const ErrorModal = (props) => {
+    return (
+    <div>
+       <div className={classes.backdrop} onClick={props.onCancel}>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+
+            <footer className={classes.actions}>
+                <Button onClick = {props.onCancel}>Okay!</Button>
+            </footer>
+        </Card>
+       </div>
+    </div>
+    )
+}
+
+export default ErrorModal;
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -4,11 +4,12 @@ import Card from '../UI/Card.js'
 import classes from '../Users/AddUser.module.css'
 import Button from '../UI/Button.js'
 import { useState } from 'react'
+import ErrorModal from '../UI/ErrorModal.js'
 
 export const AddUsers = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-
+    const [isError,setIsError] = useState(false);
 
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
@@ -19,8 +20,18 @@ export const AddUsers = (props) => {
     }
     const addUserHandler = (event) =>{
         event.preventDefault();
-        if(enteredUsername.trim().length === 0 || +enteredAge <= 0){
-            console.log("invalid Data !");
+        if(enteredUsername.trim().length === 0 ){
+            setIsError({
+                title: "Error!",
+                message: "You have to enter a username. "
+            })
+            return;
+        }
+        if(+enteredAge <= 15){
+            setIsError({
+                title: "Error!",
+                message: "Age must be above 15."
+            })
             return;
         }
 
@@ -31,24 +42,31 @@ export const AddUsers = (props) => {
 
     }
 
+    const closeModal = () => {
+        setIsError(false);
+    }
+
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-             <label htmlFor="username">Username</label>
-             <input
-                id="username"
-                type = "text"
-                value = {enteredUsername}
-                onChange={usernameChangeHandler}/>
-             <label htmlFor="">age (Years)</label>
-             <input 
-                id="username"
-                type="number" 
-                value = {enteredAge}
-                onChange={ageChangeHandler}></input>
-             <Button type = "submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            {isError &&<ErrorModal title = {isError.title} message = {isError.message} onCancel={closeModal}></ErrorModal>}
+          <Card className={classes.input}>
+              <form onSubmit={addUserHandler}>
+               <label htmlFor="username">Username</label>
+               <input
+                  id="username"
+                  type = "text"
+                  value = {enteredUsername}
+                  onChange={usernameChangeHandler}/>
+               <label htmlFor="">age (Years)</label>
+               <input 
+                  id="username"
+                  type="number" 
+                  value = {enteredAge}
+                  onChange={ageChangeHandler}></input>
+               <Button type = "submit">Add User</Button>
+              </form>
+          </Card>
+        </div>
     )
 }
 
